9b005d20a5efc659b743269c983debb7a53e292d
kr-xyriin <aschauhan8941@gmail.com>
2025-09-15 00:57:32 +0530

    Fixed a typo

--- 9.c
+++ 9.c
@@ -4,11 +4,11 @@
 #include <math.h>
 int main()
 {
-    float principal, rate, time, simple_interest, compound_interest;
-    printf("Enter principal amount, rate of interest and time (in years) separated by spaces: ");
-    scanf("%f %f %f", &principal, &rate, &time);
-    simple_interest = (principal * rate * time) / 100;
-    compound_interest = principal * (pow((1 + rate / 100), time)) - principal;
+    float principle, rate, time, simple_interest, compound_interest;
+    printf("Enter principle amount, rate of interest and time (in years) separated by spaces: ");
+    scanf("%f %f %f", &principle, &rate, &time);
+    simple_interest = (principle * rate * time) / 100;
+    compound_interest = principle * (pow((1 + rate / 100), time)) - principle;
     printf("Simple Interest: %.2f\n", simple_interest);
     printf("Compound Interest: %.2f\n", compound_interest);
     return 0;
